4bd919baf5095f388f00ac5c2e25ebba96898e5f
michelml <michmoreau.l@gmail.com>
2019-09-07 17:12:55 +0000

    commit progress

--- loader.py
+++ loader.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[4]:
+# In[5]:
 
 
 import sys
@@ -18,36 +18,36 @@ from torchvision import transforms
 
 # # Dataset Class
 
-# In[2]:
+# In[6]:
 
 
 class ImageDS(D.Dataset):
     normalize = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize(mean=(-2.1120, -2.0382, -1.8050), std=(0.0031, 0.0035, 0.0036))
+        transforms.Normalize(mean=(794.9223, 685.4205, 605.1915), std=(58.8981, 57.5124, 57.9897))
     ])
 
-    def __init__(self, df):
-        self.df = df
+    def __init__(self, dx, dy, small=False):
+        self.dx = dx
+        self.dy = dy
+        self.small = 'sm' if small else ''
 
     def __len__(self):
-        return len(self.df)
+        return len(self.dx)
 
     def __getitem__(self, idx):
-        path = f'/storage/aptosplus/{self.df.loc[idx, "ds"]}/{self.df.loc[idx, "id_code"]}.png'
+        path = f'/storage/aptosplus/{self.dx.iloc[idx]["ds"]}{self.small}/{self.dx.iloc[idx]["id_code"]}.png'
         image = Image.open(path)
-        label = torch.tensor(self.df.loc[idx, 'diagnosis'])
 
-        return ImageDS.normalize(image), label
+        return ImageDS.normalize(image), torch.tensor(self.dy.iloc[idx], dtype=torch.float)
 
 
-# In[6]:
+# In[7]:
 
 
 class ImageDS_Stats(D.Dataset):
-    normalize = transforms.Compose([
+    transform = transforms.Compose([
         transforms.ToTensor(),
-#         transforms.Normalize(mean=(123.68, 116.779, 103.939), std=(58.393, 57.12, 57.375))
     ])
 
     def __init__(self, df):
@@ -62,11 +62,5 @@ class ImageDS_Stats(D.Dataset):
         image = image.resize((512, 512))
         label = torch.tensor(self.df.loc[idx, 'diagnosis'])
 
-        return ImageDS.normalize(image), label
-
-
-# In[ ]:
-
-
-
+        return ImageDS.transform(image), label
 
